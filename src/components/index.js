5ec067703954b37d1fc3ea67c977d303e3bc23bd
drew <andrew.wr.evans@gmail.com>
2021-11-05 15:46:32 +0100

    added Nav components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as WelcomeHeader } from "./WelcomeHeader";
 export { default as DailyActivityIcon } from "./DailyActivityIcon";
+export { default as HorizontalNav } from "./HorizontalNav";
+export { default as VerticalNav } from "./VerticalNav";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import { DailyActivityIcon } from "../components/DailyActivityIcon";
 import { WelcomeHeader } from "../components/WelcomeHeader";
+import { HorizontalNav } from "../components/HorizontalNav";
+import { VerticalNav } from "../components/VerticalNav";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { faDrumstickBite } from "@fortawesome/free-solid-svg-icons";
 import { faAppleAlt } from "@fortawesome/free-solid-svg-icons";
 import { faHamburger } from "@fortawesome/free-solid-svg-icons";
-
+/**
+ * *Images imported from fontawesome for daily Icons
+ * *passed as props
+ */
 const fireIcon = <FontAwesomeIcon icon={faFire} />;
 const meatIcon = <FontAwesomeIcon icon={faDrumstickBite} />;
 const appleIcon = <FontAwesomeIcon icon={faAppleAlt} />;
@@ -14,8 +20,9 @@ const hamburgerIcon = <FontAwesomeIcon icon={faHamburger} />;
 export const Home = () => {
   return (
     <>
+      <HorizontalNav />
+      <VerticalNav />
       <WelcomeHeader name='John' />
-
       <DailyActivityIcon
         icon={fireIcon}
         metricAbv='kCal'
